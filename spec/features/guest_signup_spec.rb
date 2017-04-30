bee2348ecda8909831491c98ff58e9d1a311631b
Chase Dunagan <cdunagan@utexas.edu>
2017-04-30 12:44:36 -0600

    add test for signing up with used email

--- spec/features/guest_signup_spec.rb
+++ spec/features/guest_signup_spec.rb
@@ -21,5 +21,22 @@ RSpec.describe 'As a guest' do
       expect(current_path).to eq(root_path)
       expect(page).to have_content("Welcome to Dokkan, Cdun!")
     end
+
+    it "cannot sign in if email is already taken" do
+      User.create(username: "cdunagan", email: "texas@gmail.com", password: "texas")
+      visit root_path
+
+      click_on "Create an Account!"
+
+      fill_in "user_username", with: "Myname"
+      fill_in "user_email", with: "texas@gmail.com"
+      fill_in "user_password", with: "texas"
+      fill_in "user_password_confirmation", with: "texas"
+
+      expect(current_path).to eq(signup_path)
+      expect(page).to have_content("some sort of error")
+
+
+    end
   end
 end
