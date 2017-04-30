cfd0613c0bb04411eeb1ac1b77baf3aa3cd94964
Chase Dunagan <cdunagan@utexas.edu>
2017-04-30 13:12:51 -0600

    creates login test

---
+++ spec/features/user_logs_in_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe "When a user visits the site" do
+  context "The user tries to log in" do
+    it "logs in successfully" do
+      User.create(username: "mouse", email: "mouse@gmail.com", password: "colorado")
+
+      visit root_path
+      click_on "Login"
+
+      expect(current_path).to eq(login_path)
+
+      fill_in "user_username", with: "mouse"
+      fill_in "user_email", with: "mouse@gmail.com"
+      fill_in "user_password", with: "colorado"
+
+      click_on "Login"
+
+      expect(current_path).to eq(root_path)
+      expect(page).to have_content("Welcome to Dokkan, mouse")
+    end
+  end
+end
