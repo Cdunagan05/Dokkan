2a261d28b24ae349621b097ed5387e0ae67544ee
Chase Dunagan <cdunagan@utexas.edu>
2017-04-30 13:35:29 -0600

    add cannot login if password and username do not match

--- spec/features/user_logs_in_spec.rb
+++ spec/features/user_logs_in_spec.rb
@@ -11,13 +11,29 @@ RSpec.describe "When a user visits the site" do
       expect(current_path).to eq(login_path)
 
       fill_in "username", with: "mouse"
-      fill_in "email", with: "mouse@gmail.com"
       fill_in "password", with: "colorado"
 
       click_on "Login"
 
       expect(current_path).to eq(root_path)
       expect(page).to have_content("Welcome to Dokkan, mouse")
+    end
+
+    it "cannot login if username does not match password" do
+      User.create(username: "mouse", email: "mouse@gmail.com", password: "colorado")
+
+      visit root_path
+      click_on "Login"
+
+      expect(current_path).to eq(login_path)
+
+      fill_in "username", with: "moose"
+      fill_in "password", with: "colorado"
+
+      click_on "Login"
+
+      expect(current_path).to eq(login_path)
+      expect(page).to have_content("Oops, something went wrong, Try logging in or signing up!")
     end
   end
 end
