cf9d4d346175756a59678d17031e055ec0e0b317
Chase Dunagan <cdunagan@utexas.edu>
2017-05-01 21:48:16 -0600

    create test for buying a card

--- app/models/card.rb
+++ app/models/card.rb
@@ -2,6 +2,6 @@ class Card < ApplicationRecord
   has_many :user_cards
   has_many :users, through: :user_cards
 
-  enum rarity: [:average, :good, :elite, :hall_of_famer]
+  enum rarity: [:normal, :good, :elite, :hall_of_famer]
   enum special: [:break_tackle, :perfect_pass, :amazing_catch, :huge_hit]
 end
