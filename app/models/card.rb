bc219f8b1c0e42b7a1cdbb0fd617e8944e9c30dc
Chase Dunagan <cdunagan@utexas.edu>
2017-05-01 21:39:31 -0600

    add enums to rarity and special

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,4 +1,7 @@
 class Card < ApplicationRecord
   has_many :user_cards
   has_many :users, through: :user_cards
+
+  enum rarity: [:average, :good, :elite, :hall_of_famer]
+  enum special: [:break_tackle, :perfect_pass, :amazing_catch, :huge_hit]
 end
