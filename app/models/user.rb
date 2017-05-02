495a64acad675fb7818ec1a6662598182dcd03e8
Chase Dunagan <cdunagan@utexas.edu>
2017-05-01 18:10:16 -0600

    add validation of username and email uniqness test and makes tests pass

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true
+  validates :username, presence: true, uniqueness: true
   validates :password, presence: true
   validates_confirmation_of :password
   has_secure_password
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,4 +4,7 @@ RSpec.describe User, type: :model do
   it {is_expected.to validate_presence_of(:username)}
   it {is_expected.to validate_presence_of(:email)}
   it {is_expected.to validate_presence_of(:password)}
+
+  it {is_expected.to validate_uniqueness_of(:email)}
+  it {is_expected.to validate_uniqueness_of(:username)}
 end
